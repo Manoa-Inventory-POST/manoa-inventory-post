ef6f979990deb9f3747fd85331936d5b9219efe4
yhanessaanne <ysales@hawaii.edu>
2023-01-23 23:21:34 -1000

    added landing banner and buttons

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Button} from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { NavLink } from 'react-router-dom';
 
 /* A simple static component to render some text for the landing page. */
-const Landing = () => (
+/*const Landing = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
@@ -17,6 +18,20 @@ const Landing = () => (
 
     </Row>
   </Container>
-);
+);*/
+
+const Landing = () => (
+  <div className="landing-page-background">
+    <h1> MANOA INVENTORY: POST</h1>
+    <div id="sign-up-button">
+      <h3>New User?</h3>
+      <Button  as={ NavLink } exact to="/signup" variant="primary" size="large">Sign up</Button>
+    </div>
+    <div id="log-in-button">
+      <h3>Already have an account?</h3>
+      <Button as={NavLink} to="/signin" variant="success" size="large">Log in</Button>
+    </div>
+  </div>
+)
 
 export default Landing;
