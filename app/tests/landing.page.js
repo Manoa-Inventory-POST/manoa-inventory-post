199d6d7cc539df41a22ad8ea0f70cf73790ee45f
yhanessaanne <ysales@hawaii.edu>
2023-02-01 11:35:45 -1000

    Revert "landing page test cafe"
    This reverts commit 686e6d8993d5b7e551d921ea7e5b242a9dd8f666.

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -1,20 +1,19 @@
-import { Selector} from 'testcafe';
-//import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
+import { Selector, t } from 'testcafe';
+import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
 
 class LandingPage {
   constructor() {
-    //this.pageId = `#${PAGE_IDS.LANDING}`;
-    this.pageId = `#landing-page`;
+    this.pageId = `#${PAGE_IDS.LANDING}`;
     this.pageSelector = Selector(this.pageId);
   }
 
   /* Asserts that this page is currently displayed. */
-  async isDisplayed(testController) {
+  async isDisplayed() {
     // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
     // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
-    const waitTime = 10;
+    const waitTime = 20;
     console.log(`Waiting ${waitTime} seconds before running LandingPage.isDisplayed().`);
-    await testController.wait(1000).expect(this.pageSelector.exists).ok();
+    await t.wait(waitTime * 1000).expect(this.pageSelector.exists).ok();
   }
 }
 
