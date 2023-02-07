6c1c6de83181c8dd6937bbe4648a8bc0186b8d09
Lydia Sollis <lydia.j.sollis@gmail.com>
2023-02-07 08:54:01 -1000

    initial crate and edit room pages

---
+++ app/imports/ui/pages/EditRoom.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import swal from 'sweetalert';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, HiddenField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { useTracker } from 'meteor/react-meteor-data';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { useParams } from 'react-router';
+import { Users } from '../../api/user/UserCollection';
+import { Room } from '../../api/room/RoomCollection';
+import { updateMethod } from '../../api/base/BaseCollection.methods';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { UserProfiles } from '../../api/user/UserProfileCollection';
+
+const bridge = new SimpleSchema2Bridge(Room._schema);
+
+/* Renders the EditStuff page for editing a single document. */
+const EditRoom = () => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id } = useParams();
+  console.log(_id);
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { doc, ready } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscription = Room.subscribeRoomAdmin();
+    // const subscription = UserProfiles.subscribe();
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the document
+    const document = Room.find({ _id }).fetch();
+    const roomToEdit = document[0];
+    console.log(document);
+    return {
+      doc: roomToEdit,
+      ready: rdy,
+    };
+  }, [_id]);
+
+  // On successful submit, insert the data.
+  const submit = (data) => {
+    const { firstName, lastName, role } = data;
+    const collectionName = UserProfiles.getCollectionName();
+    const updateData = { id: _id, firstName, lastName, role };
+    updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => swal('Success', 'User updated successfully', 'success'));
+  };
+
+  return (
+      <Container className="py-3">
+        <Row className="justify-content-center">
+          <Col xs={5}>
+            <Col className="text-center"><h2>Update Room</h2></Col>
+            <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+              <Card>
+                <Card.Body>
+                  <TextField name="num" />
+                  <TextField name="description" />
+                  <TextField name="status" />
+                  <SubmitField value="Submit" />
+                  <ErrorsField />
+                </Card.Body>
+              </Card>
+            </AutoForm>
+          </Col>
+        </Row>
+      </Container>
+  );
+};
+
+export default EditRoom;
