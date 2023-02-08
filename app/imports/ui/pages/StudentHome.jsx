82a5539e3bfeb94335e0366aa9f4bcea5d2df77b
zhixinli <98064045+zhixinliuh@users.noreply.github.com>
2023-02-08 10:03:01 -1000

    updated student homepage

--- app/imports/ui/pages/StudentHome.jsx
+++ app/imports/ui/pages/StudentHome.jsx
@@ -1,97 +1,32 @@
 import React from 'react';
-import { Card, Col, Container, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 const StudentHome = () => (
   <div>
-    <div className="landing-page-background">
+    <div className="student-home-page-background">
       <h1> MĀNOA INVENTORY: POST</h1>
       <div id="about-summary">
-        <h4>This is a HOME of ICS STUDENT</h4>
+        <h3>This is a HOME of ICS STUDENT</h3>
       </div>
     </div>
     <Container id="landing-page">
       {/* eslint-disable-next-line max-len */}
-      <h6 id="about-landing">Mānoa Inventory: POST is a web application tool to allow many users and roles to find what they are looking for on the third floor of POST building at UH Manoa. With the various spaces and inventory it has to
-        offer, we strive to make the reservation and management process an easy one. Features include but are not limited to:
-      </h6>
-      <Row className="align-middle text-center">
-        <Col className="d-flex flex-column justify-content-center">
-          <Row xs={1} md={2}>
-            <div className="summary-card">
-              <Card>
-                <Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>
-                      <Card.Title>For Students</Card.Title>
-                    </ListGroupItem>
-                    <Card.Text className="role- list">
-                      <ul>
-                        <li>Request room reservations based upon availability and approval</li>
-                        <li>View the offices your professors are located in</li>
-                        <li>Request a cubicle (TA/RA)</li>
-                      </ul>
-                    </Card.Text>
-                  </ListGroup>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="summary-card">
-              <Card>
-                <Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>
-                      <Card.Title>For Office Staff</Card.Title>
-                    </ListGroupItem>
-                    <Card.Text className="role- list">
-                      <ul>
-                        <li>Find out which rooms has what tools and materials</li>
-                        <li>Assign and move occupants to a different room</li>
-                        <li>Responsible for space management</li>
-                      </ul>
-                    </Card.Text>
-                  </ListGroup>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="summary-card">
-              <Card>
-                <Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>
-                      <Card.Title>For IT Support</Card.Title>
-                    </ListGroupItem>
-                    <Card.Text className="role- list">
-                      <ul>
-                        <li>View which rooms has functional ports</li>
-                        <li>Update the functionality status of equipment and materials</li>
-                        <li>Check up on network jacks and phone jacks</li>
-                      </ul>
-                    </Card.Text>
-                  </ListGroup>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="summary-card">
-              <Card>
-                <Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>
-                      <Card.Title>For Faculty</Card.Title>
-                    </ListGroupItem>
-                    <Card.Text>
-                      <ul>
-                        <li>View what rooms are available by date and time</li>
-                        <li>View which rooms have which equipment</li>
-                        <li>Reserve rooms</li>
-                      </ul>
-                    </Card.Text>
-                  </ListGroup>
-                </Card.Body>
-              </Card>
-            </div>
-          </Row>
-        </Col>
-      </Row>
+      <section>
+        <div className="container px-5 my-5">
+          <div className=" mb-5 mb-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i className="bi bi-collection" /></div>
+            <h2 className="h4 fw-bolder text-center">Searching for Faculty</h2>
+            <br />
+            <p>Allows your self to search for faculty information and office hours on the 3rd floor of POST Building.
+              "As an undergraduate student I want to be able to look up my professor by name to find out his or her office hours and location."
+            </p>
+            <a className="text-decoration-none" href="/faculty">
+              Click Here
+              <i className="bi bi-arrow-right" />
+            </a>
+          </div>
+        </div>
+      </section>
     </Container>
   </div>
 );
