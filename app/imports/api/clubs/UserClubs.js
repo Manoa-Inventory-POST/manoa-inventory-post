6c77c6ea21d73135d273bada61ab41ecc8093477
justinjandoc <102justinjandoc@gmail.com>
2023-02-15 11:32:15 -1000

    Update UserClubs.js

--- app/imports/api/clubs/UserClubs.js
+++ app/imports/api/clubs/UserClubs.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import { check } from 'meteor/check';
-import { Roles } from 'meteor/alanning:roles';
 import BaseCollection from '../base/BaseCollection';
 import { ROLE } from '../role/Role';
 
