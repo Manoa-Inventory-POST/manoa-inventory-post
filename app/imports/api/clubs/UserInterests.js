e947ccb61862caee1c89635da5d1aa52e94d88fe
justinjandoc <102justinjandoc@gmail.com>
2023-02-15 12:02:52 -1000

    Revert "Issue #210 Updated UserInterests.js"
    This reverts commit cb5b9c864872215bc37e5cfe5cf611d998387d72.

--- app/imports/api/clubs/UserInterests.js
+++ app/imports/api/clubs/UserInterests.js
@@ -1,139 +1,25 @@
-import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
-import { check } from 'meteor/check';
-import { Roles } from 'meteor/alanning:roles';
-import BaseCollection from '../base/BaseCollection';
-import { ROLE } from '../role/Role';
+import { Tracker } from 'meteor/tracker';
 
-export const userInterestsPublications = {
-  userInterestsPub: 'userInterestsPub',
-  userInterestsPubAdmin: 'userInterestsPubAdmin',
-};
-
-class UserInterestsCollection extends BaseCollection {
+/** Encapsulates state and variable values for this collection. */
+class ProfilesInterestsCollection {
   constructor() {
-    super('UserInterests', new SimpleSchema({
+    // The name of this collection.
+    this.name = 'ProfilesInterestsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
       profile: String,
-      interest: String,
-    }));
-  }
-
-  /**
-   * Defines a new UserInterests item.
-   * @return {String} the docID of the new document.
-   * @param profile
-   * @param interest
-   */
-  define({ profile, interest }) {
-    const docID = this._collection.insert({
-      profile,
-      interest,
-    });
-    return docID;
-  }
-
-  /**
-   * Updates the given document.
-   * @param docID the id of the document to update.
-   * @param profile the new profile (optional).
-   * @param interest the new interest (optional).
-   * @returns never
-   */
-  update(docID, { profile, interest }) {
-    const updateData = {};
-    if (profile) {
-      updateData.profile = profile;
-    }
-    if (interest) {
-      updateData.interest = interest;
-    }
-    this._collection.update(docID, { $set: updateData });
-  }
-
-  /**
-   * A stricter form of remove that throws an error if the document or docID could not be found in this collection.
-   * @param { String | Object } name A document or docID in this collection.
-   * @returns true
-   */
-  removeIt(name) {
-    const doc = this.findDoc(name);
-    check(doc, Object);
-    this._collection.remove(doc._id);
-    return true;
-  }
-
-  /**
-   * Default publication method for entities.
-   * It publishes the entire collection for admin and just the stuff associated to an owner.
-   */
-  publish() {
-    if (Meteor.isServer) {
-      // get the StuffCollection instance.
-      const instance = this;
-      /** This subscription publishes only the documents associated with the logged-in user */
-      Meteor.publish(userInterestsPublications.userInterestsPub, function publish() {
-        if (this.userId) {
-          const username = Meteor.users.findOne(this.userId).username;
-          return instance._collection.find({ owner: username });
-        }
-        return this.ready();
-      });
-
-      /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-      Meteor.publish(userInterestsPublications.userInterestsPubAdmin, function publish() {
-        if (this.userId && Roles.userIsInRole(this.userId, ROLE.ADMIN)) {
-          return instance._collection.find();
-        }
-        return this.ready();
-      });
-    }
-  }
-
-  /**
-   * Subscription method for stuff owned by the current user.
-   */
-  subscribeUserInterests() {
-    if (Meteor.isClient) {
-      return Meteor.subscribe(userInterestsPublications.userInterestsPub);
-    }
-    return null;
-  }
-
-  /**
-   * Subscription method for admin users.
-   * It subscribes to the entire collection.
-   */
-  subscribeUserInterestsAdmin() {
-    if (Meteor.isClient) {
-      return Meteor.subscribe(userInterestsPublications.userInterestsPub);
-    }
-    return null;
-  }
-
-  /**
-   * Default implementation of assertValidRoleForMethod. Asserts that userId is logged in as an Admin or User.
-   * This is used in the define, update, and removeIt Meteor methods associated with each class.
-   * @param userId The userId of the logged in user. Can be null or undefined
-   * @throws { Meteor.Error } If there is no logged in user, or the user is not an Admin or User.
-   */
-  assertValidRoleForMethod(userId) {
-    this.assertRole(userId, [ROLE.ADMIN, ROLE.USER, ROLE.STUDENT, ROLE.OFFICE, ROLE.FACULTY, ROLE.ITSUPPORT, ROLE.ADVISOR]);
-  }
-
-  /**
-   * Returns an object representing the definition of docID in a format appropriate to the restoreOne or define function.
-   * @param docID
-   * @return { profile, interest}
-   */
-  dumpOne(docID) {
-    const doc = this.findDoc(docID);
-    const profile = doc.profile;
-    const interest = doc.interest;
-    return { profile, interest };
+      interests: String,
+    }, { tracker: Tracker });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
   }
 }
 
-/**
- * Provides the singleton instance of this class to all other entities.
- */
-export const UserInterests = new UserInterestsCollection();
+export const UserInterests = new ProfilesInterestsCollection();
