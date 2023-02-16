06ee256ff64e61e46b081ef609bf4ec8ccfa3810
Lydia Sollis <lydia.j.sollis@gmail.com>
2023-02-16 12:34:30 -1000

    initialize clubs collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/StuffCollection';
+// import { Clubs } from
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -15,3 +16,13 @@ if (Stuffs.count() === 0) {
     Meteor.settings.defaultData.map(data => addData(data));
   }
 }
+
+/*
+// Initialize the ClubsCollection if empty.
+if (Clubs.count() === 0) {
+  if (Meteor.settings.defaultClubs) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultClubs.map(clubs => addClubs(clubs));
+  }
+}
+ */
