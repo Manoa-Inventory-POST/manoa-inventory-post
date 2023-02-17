e6a886699a9888f03bb0cffb132ba83120d57674
shannonkam <skam7@hawaii.edu>
2023-02-16 22:42:25 -1000

    Fixed eslint errors

--- app/imports/ui/pages/CreateUser.jsx
+++ app/imports/ui/pages/CreateUser.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import swal from 'sweetalert';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, HiddenField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import { useTracker } from 'meteor/react-meteor-data';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { useParams } from 'react-router';
-import { Users } from '../../api/user/UserCollection';
+// import { Users } from '../../api/user/UserCollection';
 import { updateMethod } from '../../api/base/BaseCollection.methods';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
@@ -16,7 +16,6 @@ const bridge = new SimpleSchema2Bridge(UserProfiles._schema);
 const EditUser = () => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const { _id } = useParams();
-  console.log(_id);
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { doc, ready } = useTracker(() => {
     // Get access to Stuff documents.
@@ -26,7 +25,6 @@ const EditUser = () => {
     // Get the document
     const document = UserProfiles.find({ _id }).fetch();
     const userToEdit = document[0];
-    console.log(document);
     return {
       doc: userToEdit,
       ready: rdy,
