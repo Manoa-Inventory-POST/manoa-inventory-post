6c2b03883c3505e2434cecbe02dff6b32402e398
justinjandoc <102justinjandoc@gmail.com>
2023-02-21 11:47:01 -1000

    Issue #232 Updated ClubInsterests

--- app/imports/api/clubs/ClubInterests.js
+++ app/imports/api/clubs/ClubInterests.js
@@ -1,25 +1,117 @@
-import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
-import { Tracker } from 'meteor/tracker';
+import { check } from 'meteor/check';
+import BaseCollection from '../base/BaseCollection';
+import { ROLE } from '../role/Role';
 
-/** Encapsulates state and variable values for this collection. */
-class ClubInterestsCollection {
+export const clubInterestsPublications = {
+  clubInterestsPub: 'clubInterestsPub',
+};
+
+class ClubInterestsCollection extends BaseCollection {
   constructor() {
-    // The name of this collection.
-    this.name = 'ClubInterestsCollection';
-    // Define the Mongo collection.
-    this.collection = new Mongo.Collection(this.name);
-    // Define the structure of each document in the collection.
-    this.schema = new SimpleSchema({
-      club: String,
+    super('ClubInterests', new SimpleSchema({
       interest: String,
-    }, { tracker: Tracker });
-    // Ensure collection documents obey schema.
-    this.collection.attachSchema(this.schema);
-    // Define names for publications and subscriptions
-    this.userPublicationName = `${this.name}.publication.user`;
-    this.adminPublicationName = `${this.name}.publication.admin`;
+      club: String,
+    }));
+  }
+
+  /**
+   * Defines a new ClubInterests item.
+   * @return {never} the docID of the new document.
+   * @param interest
+   * @param club
+   */
+  define({ interest, club }) {
+    const docID = this._collection.insert({
+      interest,
+      club,
+    });
+    return docID;
+  }
+
+  /**
+   * Updates the given document.
+   * @param docID the id of the document to update.
+   * @param interest the new interest (optional).
+   * @param club the new club (optional).
+   * @returns never
+   */
+  update(docID, { interest, club }) {
+    const updateData = {};
+    if (interest) {
+      updateData.interest = interest;
+    }
+    if (club) {
+      updateData.club = club;
+    }
+    this._collection.update(docID, { $set: updateData });
+  }
+
+  /**
+   * A stricter form of remove that throws an error if the document or docID could not be found in this collection.
+   * @param { String | Object } name A document or docID in this collection.
+   * @returns true
+   */
+  removeIt(name) {
+    const doc = this.findDoc(name);
+    check(doc, Object);
+    this._collection.remove(doc._id);
+    return true;
+  }
+
+  /**
+   * Default publication method for entities.
+   * It publishes the entire collection for admin and just the stuff associated to an owner.
+   */
+  publish() {
+    if (Meteor.isServer) {
+      // get the StuffCollection instance.
+      const instance = this;
+      /** This subscription publishes only the documents associated with the logged-in user */
+      Meteor.publish(clubInterestsPublications.clubInterestsPub, function publish() {
+        if (this.userId) {
+          return instance._collection.find({ });
+        }
+        return this.ready();
+      });
+    }
+  }
+
+  /**
+   * Subscription method for stuff owned by the current user.
+   */
+  subscribeClubInterests() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(clubInterestsPublications.clubInterestsPub);
+    }
+    return null;
+  }
+
+  /**
+   * Default implementation of assertValidRoleForMethod. Asserts that userId is logged in as an Admin or User.
+   * This is used in the define, update, and removeIt Meteor methods associated with each class.
+   * @param userId The userId of the logged in user. Can be null or undefined
+   * @throws { Meteor.Error } If there is no logged in user, or the user is not an Admin or User.
+   */
+  assertValidRoleForMethod(userId) {
+    this.assertRole(userId, [ROLE.ADMIN, ROLE.USER, ROLE.STUDENT, ROLE.OFFICE, ROLE.FACULTY, ROLE.ITSUPPORT, ROLE.ADVISOR]);
+  }
+
+  /**
+   * Returns an object representing the definition of docID in a format appropriate to the restoreOne or define function.
+   * @param docID
+   * @return { interest, club}
+   */
+  dumpOne(docID) {
+    const doc = this.findDoc(docID);
+    const interest = doc.interest;
+    const club = doc.club;
+    return { interest, club };
   }
 }
 
+/**
+ * Provides the singleton instance of this class to all other entities.
+ */
 export const ClubInterests = new ClubInterestsCollection();
