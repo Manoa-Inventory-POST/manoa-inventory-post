ae45906337ad6c9c9786ae9910e7bfd5df9a16d9
justinjandoc <102justinjandoc@gmail.com>
2023-02-21 19:29:26 -1000

    Fixed ESLint errors

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -1,20 +1,21 @@
-import { Selector, t} from 'testcafe';
-import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
+import { Selector, t } from 'testcafe';
+// import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
 
 class LandingPage {
   constructor() {
-    this.pageId = `#landing-page`;
+    this.pageId = '#landing-page';
     this.pageSelector = Selector(this.pageId);
   }
 
   /* Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
+    this._testController = testController;
     // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
     // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
     const waitTime = 20;
     console.log(`Waiting ${waitTime} seconds before running LandingPage.isDisplayed().`);
     await t.wait(waitTime * 1000).expect(this.pageSelector.exists).ok();
-   // await testController.wait(20000).expect(this.pageSelector.exists).ok();
+    // await testController.wait(20000).expect(this.pageSelector.exists).ok();
   }
 }
 
