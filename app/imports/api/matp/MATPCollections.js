cfe40e25a23975accac2f1b597efad3e8dfdae22
justinjandoc <102justinjandoc@gmail.com>
2023-02-25 14:07:02 -1000

    Revert Revert

--- app/imports/api/matp/MATPCollections.js
+++ app/imports/api/matp/MATPCollections.js
@@ -18,6 +18,8 @@ import { ClubOfficer } from '../clubs/ClubOfficer';
 import { UserClubs } from '../clubs/UserClubs';
 import { UserInterests } from '../clubs/UserInterests';
 import { Room } from '../room/RoomCollection';
+import { Clubs } from '../clubs/Clubs';
+import { ClubAdvisor } from '../clubs/ClubAdvisor';
 
 class MATPClass {
   collections;
@@ -48,6 +50,7 @@ class MATPClass {
       UserClubs,
       UserInterests,
       Room,
+      Clubs,
     ];
     /*
      * A list of collection class instances in the order required for them to be sequentially loaded from a file.
@@ -72,6 +75,7 @@ class MATPClass {
       UserClubs,
       UserInterests,
       Room,
+      ClubAdvisor,
     ];
 
     /*
@@ -94,7 +98,7 @@ class MATPClass {
     // console.log('MATPCollections', collectionName, this.collectionAssociation);
     const collection = this.collectionAssociation[collectionName];
     if (!collection) {
-      throw new Meteor.Error(`Called MARTPCollections.getCollection with unknown collection name: ${collectionName}`);
+      throw new Meteor.Error(`Called MATPCollections.getCollection with unknown collection name: ${collectionName}`);
     }
     return collection;
   }
