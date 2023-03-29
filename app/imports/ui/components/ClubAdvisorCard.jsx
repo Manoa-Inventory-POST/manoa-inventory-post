3b711bfa0fd3e3ef8f8e78efd5b3710c3b2aadcc
justinjandoc <102justinjandoc@gmail.com>
2023-03-29 10:55:33 -1000

    Issue #222 Updated Club Officer & Advisor cards

--- app/imports/ui/components/ClubAdvisorCard.jsx
+++ app/imports/ui/components/ClubAdvisorCard.jsx
@@ -2,31 +2,26 @@ import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-/** Renders a single ClubAdminCard. See pages/ClubPage & pages/AdminClubPage. */
-class ClubAdvisorCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        <Image src={this.props.member.picture} wrapped ui={false} />
-        <Card.Content>
-          <Card.Header>{this.props.member.firstName} {this.props.member.lastName}</Card.Header>
-          <Card.Meta>
-            <span className="email">{this.props.member.email}</span>
-          </Card.Meta>
-        </Card.Content>
-      </Card>
-    );
-  }
-}
+/* Renders a single ClubAdvisorard. */
+const ClubAdvisorCard = ({ advisor }) => (
+  <Card>
+    <Image src={advisor.picture} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{advisor.firstName} {advisor.lastName}</Card.Header>
+      <Card.Meta>
+        <span className="email">{advisor.email}</span>
+      </Card.Meta>
+    </Card.Content>
+  </Card>
+);
 
 // Require a document to be passed to this component.
 ClubAdvisorCard.propTypes = {
-  member: PropTypes.shape({
+  advisor: PropTypes.shape({
     firstName: PropTypes.string,
     lastName: PropTypes.string,
     email: PropTypes.string,
     picture: PropTypes.string,
-    role: PropTypes.string,
   }).isRequired,
 };
 
