2a0b693bb9e0c40b8726a0bb49bd67616f8f5867
luoxiao11 <luoxiao.rf@gmail.com>
2023-04-02 23:14:56 -1000

    UPDATE4

--- app/imports/ui/pages/ProfileUpdate.jsx
+++ app/imports/ui/pages/ProfileUpdate.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router';
 import { AutoForm, BoolField, ErrorsField, HiddenField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import swal from 'sweetalert';
 import { Phone } from '../../api/room/Phone';
 import { Clubs } from '../../api/clubs/Clubs';
 import { Room } from '../../api/room/RoomCollection';
@@ -18,12 +19,13 @@ import { StudentProfiles } from '../../api/user/StudentProfileCollection';
 import { OfficeProfiles } from '../../api/user/OfficeProfileCollection';
 import { ITSupportProfiles } from '../../api/user/ITSupportProfileCollection';
 import { OccupantRoom } from '../../api/room/OccupantRoom';
+import { updateMethod } from '../../api/base/BaseCollection.methods';
 
 const ProfileUpdate = () => {
 
   const _id = Meteor.user()._id;
   console.log(_id);
-  const { ready, userProfile } = useTracker(() => {
+  const { ready, userProfile, roomValues, interestNames, clubNames } = useTracker(() => {
     const subPhone = Phone.subscribePhone();
     const subClubs = Clubs.subscribeClubs();
     const subscriptionRooms = Room.subscribeRoom();
@@ -40,8 +42,8 @@ const ProfileUpdate = () => {
         && subUser.ready() && subAdmin.ready() && subFaculty.ready() && subStudent.ready() && subOffice.ready()
         && subIT.ready() && subOccRoom.ready() && subPhone.ready();
 
-    //const subscription = FacultyProfiles.subscribeFaculty();
-    //const facultyProfiles = FacultyProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
+    // const subscription = FacultyProfiles.subscribeFaculty();
+    // const facultyProfiles = FacultyProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
     const docUser = UserProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
     const docAdmin = AdminProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
     const docFaculty = FacultyProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
@@ -60,16 +62,17 @@ const ProfileUpdate = () => {
       userProfile = docFaculty[0];
       console.log('faculty switch');
       const email = userProfile.email;
+      // attach office
       let roomArr = OccupantRoom.find({ email: email }, {}).fetch();
       const occRoomIdArr = roomArr.map(obj => obj._id);
       console.log(occRoomIdArr);
       roomArr = roomArr.map(item => item.room);
-      roomArr = roomArr.map(item => item.split(' '));
-      roomArr = roomArr.map(item => ((item.length > 1) ? item[1] : item[0]));
+     // roomArr = roomArr.map(item => item.split(' '));
+      //roomArr = roomArr.map(item => ((item.length > 1) ? item[1] : item[0]));
       userProfile.office = roomArr;
       userProfile.occupantRoomIds = occRoomIdArr;
       console.log(userProfile.office);
-
+      // attach phone
       let phoneArr = Phone.find({ email: email }, {}).fetch();
       console.log(phoneArr);
       const phoneIdArr = phoneArr.map(item => item._id);
@@ -83,6 +86,25 @@ const ProfileUpdate = () => {
       userProfile.phones = phoneArr;
       userProfile.phoneIds = phoneIdArr;
       console.log(userProfile);
+      // attach mentor
+      const advisor = `${userProfile.firstName} ${userProfile.lastName}`;
+      console.log(advisor);
+      let clubArr = ClubAdvisor.find({ advisor: advisor }, {}).fetch();
+      const clubAdvisorIds = clubArr.map(Item => Item._id);
+      userProfile.clubAdvisorIds = clubAdvisorIds;
+      console.log(clubArr);
+      clubArr = clubArr.map(item => item.club);
+
+      console.log(clubArr);
+      if (clubArr.length > 0) {
+        userProfile.clubAdvisor = true;
+      } else {
+        userProfile.clubAdvisor = false;
+      }
+      userProfile.clubs = clubArr;
+      console.log(userProfile.clubs);
+
+      console.log(userProfile);
 
     } else if (docStudent.length > 0) {
       userProfile = docStudent[0];
@@ -94,14 +116,19 @@ const ProfileUpdate = () => {
       console.log('user not found');
     }
 
+    const roomEntries = Room.find({}, { sort: { num: 1 } }).fetch();
+    const interestEntries = Interests.find({}, {}).fetch();
+    const clubEntries = Clubs.find({}, {}).fetch();
+
     return {
       userProfile: userProfile,
+      roomValues: roomEntries.map(Item => Item.room),
+      interestNames: interestEntries.map(Item => Item.interest),
+      clubNames: clubEntries.map(Item => Item.name),
       ready: rdy,
     };
   }, []);
-  //console.log(userProfile);
-  //console.log('sss');
-
+  console.log(roomValues, interestNames, clubNames, ready);
   const UserProfileSchema = new SimpleSchema({
     email: String,
     firstName: String,
@@ -113,9 +140,8 @@ const ProfileUpdate = () => {
     'phoneIds.$': { type: String, optional: true },
     occupantRoomIds: { type: Array, optional: true },
     'occupantRoomIds.$': { type: String, optional: true },
-    role: { type: String },
-    rooms: { type: Array, label: 'Office(s)', optional: true },
-    'rooms.$': { type: String },
+    role: { type: String, optional: true },
+    office: { type: String, label: 'Office(s)', optional: true },
     phones: { type: String, label: 'Phone Numbers', optional: true },
     officeHours: { type: String, optional: true },
     picture: { type: String, optional: true },
@@ -126,20 +152,87 @@ const ProfileUpdate = () => {
     undergraduate: { type: Boolean, defaultValue: false },
     clubAdvisor: { type: Boolean, defaultValue: false },
     clubs: { type: Array, label: 'Clubs', optional: true },
-    'clubs.$': { type: String, optional: true },
+    'clubs.$': { type: String, allowedValues: clubNames, optional: true },
     interests: { type: Array, label: 'Interests', optional: true },
-    'interests.$': { type: String, optional: true },
+    'interests.$': { type: String, allowedValues: interestNames, optional: true },
   });
+
   const bridge = new SimpleSchema2Bridge(UserProfileSchema);
+
+  const submit = (data) => {
+    const { email, phones, phoneIds, interests, officeHours, role, office } = data;
+    const phonesArray = phones.split(', ');
+    office = office.toString();
+    let collectionName;
+    let updateData;
+
+    switch (role) {
+    case 'ADMIN':
+      updateData = { email, phones: phonesArray };
+      collectionName = AdminProfiles.getCollectionName();
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'Admin updated successfully', 'success');
+        });
+      break;
+
+    case 'FACULTY':
+      updateData = { email, phones: phonesArray, officeHours };
+      collectionName = FacultyProfiles.getCollectionName();
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'Admin updated successfully', 'success');
+        });
+      break;
+    case 'USER':
+      updateData = { email, phones: phonesArray };
+      collectionName = UserProfiles.getCollectionName();
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'Admin updated successfully', 'success');
+        });
+      break;
+    case 'STUDENT':
+      updateData = { email, phones: phonesArray };
+      collectionName = StudentProfiles.getCollectionName();
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'Admin updated successfully', 'success');
+        });
+      break;
+    case 'OFFICE':
+      updateData = { email, phones: phonesArray };
+      collectionName = OfficeProfiles.getCollectionName();
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'Admin updated successfully', 'success');
+        });
+      break;
+    case 'ITSUPPORT':
+      updateData = { email, phones: phonesArray };
+      collectionName = ITSupportProfiles.getCollectionName();
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'Admin updated successfully', 'success');
+        });
+      break;
+    }
+  };
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col className="col-lg-10">
-          <Col className="text-center"><h2>{ userProfile.firstName }&apos;s PROFILE</h2></Col>
+          <Col className="text-center"><h2>{ userProfile.firstName }&apos;s Profile</h2></Col>
           {/*
            <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
 */}
-          <AutoForm schema={bridge}>
+          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={userProfile}>
             <Card>
               <Card.Body>
                 <div className="row">
@@ -165,23 +258,23 @@ const ProfileUpdate = () => {
                   />
                 </div>
                 <div className="row">
-                  <SelectField className="col-md-6" name="role" placeholder="select role (required)" />
+                  <TextField className="col-md-6" name="office" placeholder="Your office rooms" readOnly />
                   <TextField className="col-md-6" name="officeHours" placeholder="Your office hours" />
                 </div>
                 <div className="row">
-                  <TextField className="col" name="picture" placeholder="picture url" />
+                  <TextField className="col-md-6" name="role" placeholder="select role (required)" readOnly/>
                   <TextField className="col" name="position" placeholder="Your position" readOnly />
                 </div>
+
                 <div className="row">
-                  <SelectField className="col-md-3" name="rooms" multiple inline />
                   <SelectField
-                    className="col-md-5"
+                    className="col-md-6"
                     name="clubs"
                     placeholder="Select any clubs you are the advisor for"
                     multiple
                   />
                   <SelectField
-                    className="col-md-4"
+                    className="col-md-6"
                     name="interests"
                     placeholder="Select your interests from the options provided"
                     multiple
@@ -195,6 +288,9 @@ const ProfileUpdate = () => {
                   <BoolField className="d-md-inline" name="graduate" inline />
                   <BoolField className="d-md-inline" name="clubAdvisor" inline />
                 </div>
+                <HiddenField name="password" value="changeme" />
+                <HiddenField name="clubAdvisorIds" />
+                <HiddenField name="phoneIds" />
                 <SubmitField value="Save" />
                 <ErrorsField />
               </Card.Body>
