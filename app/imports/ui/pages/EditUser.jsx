ea5604967466f4db8208176e9fdd6bb5f869e07e
Kai <hekai@hawaii.edu>
2023-04-04 17:31:11 -1000

    Revert "Merge pull request #388 from Manoa-Inventory-POST/issue-371"
    This reverts commit d8b222ccf5bc7543de945823e69832f207a624ce, reversing
    changes made to 8ad9311150134ed86361c0cb811ef73bb545df3b.

--- app/imports/ui/pages/EditUser.jsx
+++ app/imports/ui/pages/EditUser.jsx
@@ -27,8 +27,6 @@ import { updateMethod } from '../../api/base/BaseCollection.methods';
 import { Interests } from '../../api/clubs/Interests';
 import { OccupantRoom } from '../../api/room/OccupantRoom';
 import { Phone } from '../../api/room/Phone';
-import { UserInterests } from '../../api/clubs/UserInterests';
-import { UserClubs } from '../../api/clubs/UserClubs';
 
 const EditUser = () => {
 
@@ -41,9 +39,7 @@ const EditUser = () => {
     const subscription = Room.subscribeRoom();
     const subClubAdvisor = ClubAdvisor.subscribeClubAdvisor();
     const subInterests = Interests.subscribeInterests();
-    const subUserInterests = UserInterests.subscribeUserInterests();
     const subUser = UserProfiles.subscribe();
-    const subUserClubs = UserClubs.subscribeUserClubs();
     const subAdmin = AdminProfiles.subscribe();
     const subFaculty = FacultyProfiles.subscribeFaculty();
     const subStudent = StudentProfiles.subscribe();
@@ -52,7 +48,7 @@ const EditUser = () => {
     const subOccRoom = OccupantRoom.subscribeOccupantRoom();
     const rdy = subscription.ready() && subClubAdvisor.ready() && subInterests.ready() && subClubs.ready()
         && subUser.ready() && subAdmin.ready() && subFaculty.ready() && subStudent.ready() && subOffice.ready()
-        && subIT.ready() && subOccRoom.ready() && subPhone.ready() && subUserInterests.ready() && subUserClubs.ready();
+        && subIT.ready() && subOccRoom.ready() && subPhone.ready();
     const docUser = UserProfiles.find({ _id }, {}).fetch();
     const docAdmin = AdminProfiles.find({ _id }, {}).fetch();
     const docFaculty = FacultyProfiles.find({ _id }, {}).fetch();
@@ -109,25 +105,6 @@ const EditUser = () => {
 
     } else if (docStudent.length > 0) {
       userToEdit = docStudent[0];
-      const email = userToEdit.email;
-
-      let interestsArr = UserInterests.find({ email: email }, {}).fetch();
-      console.log(interestsArr);
-      const interestsIdArr = interestsArr.map(item => item._id);
-      interestsArr = interestsArr.map(item => item.interest);
-      console.log(interestsArr);
-      userToEdit.interests = interestsArr;
-      userToEdit.interestIds = interestsIdArr;
-
-      let clubsArr = UserClubs.find({ email: email }, {}).fetch();
-      console.log(clubsArr);
-      const clubsIdArr = clubsArr.map(item => item._id);
-      console.log(clubsIdArr);
-      clubsArr = clubsArr.map(item => item.club);
-      console.log(clubsArr);
-      userToEdit.clubs = clubsArr;
-      userToEdit.clubIds = clubsIdArr;
-
     } else if (docOffice.length > 0) {
       userToEdit = docOffice[0];
     } else if (docIT.length > 0) {
@@ -191,12 +168,8 @@ const EditUser = () => {
     clubAdvisor: { type: Boolean, defaultValue: false },
     clubs: { type: Array, label: 'Clubs', optional: true },
     'clubs.$': { type: String, allowedValues: clubNames, optional: true },
-    clubIds: { type: Array, optional: true },
-    'clubIds.$': { type: String, optional: true },
     interests: { type: Array, label: 'Interests', optional: true },
     'interests.$': { type: String, allowedValues: interestNames, optional: true },
-    interestIds: { type: Array, optional: true },
-    'interestIds.$': { type: String, optional: true },
   });
 
   const bridge = new SimpleSchema2Bridge(UserFormSchema);
@@ -204,8 +177,7 @@ const EditUser = () => {
   // On successful submit, insert the data.
   const submit = (data) => {
     console.log('submit');
-    const { firstName, lastName, email, role, rooms, occupantRoomIds, phones, phoneIds, clubAdvisor, clubs, TA, RA, undergraduate, graduate, officeHours, position, picture, interests, clubAdvisorIds, clubIds, interestIds } = data;
-    console.log(data);
+    const { firstName, lastName, email, role, rooms, occupantRoomIds, phones, phoneIds, clubAdvisor, clubs, TA, RA, undergraduate, graduate, officeHours, position, picture, interests, clubAdvisorIds } = data;
     const phonesArray = phones.split(', ');
     console.log(data);
     let collectionName;
@@ -214,17 +186,6 @@ const EditUser = () => {
     console.log(updateData);
 
     switch (role) {
-    case 'STUDENT':
-      console.log('STUDENT SWITCH');
-      collectionName = StudentProfiles.getCollectionName();
-      console.log(interests);
-      updateData = { id: _id, firstName, lastName, email, TA, RA, graduate, undergraduate, clubs, clubIds, interests, interestIds };
-      updateMethod.callPromise({ collectionName, updateData })
-        .catch(error => swal('Error', error.message, 'error'))
-        .then(() => {
-          swal('Success', 'User updated successfully', 'success');
-        });
-      break;
     case 'ADMIN':
       console.log('ADMIN SWITCH');
       collectionName = AdminProfiles.getCollectionName();
@@ -237,7 +198,7 @@ const EditUser = () => {
     case 'FACULTY':
       console.log('FACULTY SWITCH');
       collectionName = FacultyProfiles.getCollectionName();
-      updateData = { id: _id, email, firstName, lastName, officeHours, position, picture, rooms, occupantRoomIds, phones: phonesArray, phoneIds, clubs, clubIds, clubAdvisorIds, clubAdvisor };
+      updateData = { id: _id, email, firstName, lastName, officeHours, position, picture, rooms, occupantRoomIds, phones: phonesArray, phoneIds, clubs, clubAdvisorIds, clubAdvisor };
       updateMethod.callPromise({ collectionName, updateData })
         .catch(error => swal('Error', error.message, 'error'))
         .then(() => {
@@ -249,6 +210,17 @@ const EditUser = () => {
       collectionName = UserProfiles.getCollectionName();
       console.log(collectionName);
       console.log(typeof collectionName);
+      updateMethod.callPromise({ collectionName, updateData })
+        .catch(error => swal('Error', error.message, 'error'))
+        .then(() => {
+          swal('Success', 'User updated successfully', 'success');
+        });
+      break;
+    case 'STUDENT':
+      console.log('STUDENT SWITCH');
+      collectionName = StudentProfiles.getCollectionName();
+      console.log(interests);
+      updateData = { id: _id, firstName, lastName, email, TA, RA, graduate, undergraduate };
       updateMethod.callPromise({ collectionName, updateData })
         .catch(error => swal('Error', error.message, 'error'))
         .then(() => {
@@ -323,8 +295,6 @@ const EditUser = () => {
                 <HiddenField name="clubAdvisorIds" />
                 <HiddenField name="phoneIds" />
                 <HiddenField name="occupantRoomIds" />
-                <HiddenField name="clubIds" />
-                <HiddenField name="interestIds" />
                 <SubmitField value="Submit" />
                 <ErrorsField />
               </Card.Body>
