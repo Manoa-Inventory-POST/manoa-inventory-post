4de879916e2df196a3b5ab726d5e8189695fb6a9
Eduardo Kho Jr <eduardok@hawaii.edu>
2023-04-10 10:49:53 -1000

    prep for form & updated from main

--- app/imports/ui/components/PortItem.jsx
+++ app/imports/ui/components/PortItem.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { Container, Image, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const handleStatus = async (portID, portStatus)
-
 const PortItem = ({ port }) => (
 
   <tr key={port._id}>
@@ -12,13 +10,7 @@ const PortItem = ({ port }) => (
     <td>{port.room}</td>
     <td>{port.side}</td>
     <td>{port.idf}</td>
-    <td>
-      <Form.Select value={port.status} onChange={() => handleStatus(port._id, portStatus)}>
-        <option value="1">Active</option>
-        <option value="2">Inactive</option>
-        <option value="3">Maintenance</option>
-      </Form.Select>
-    </td>
+    <td>{port.status}</td>
   </tr>
 );
 
