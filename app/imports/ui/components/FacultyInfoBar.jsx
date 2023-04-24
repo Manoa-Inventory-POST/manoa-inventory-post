46d254fdee735731b60dd4a6bdc17fa920213c70
Lydia Sollis <lydia.j.sollis@gmail.com>
2023-04-23 19:27:50 -1000

    link faculty profile page to backend

--- app/imports/ui/components/FacultyInfoBar.jsx
+++ app/imports/ui/components/FacultyInfoBar.jsx
@@ -1,24 +1,64 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Card, ListGroup } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { FacultyProfiles } from '../../api/user/FacultyProfileCollection';
+import { Room } from '../../api/room/RoomCollection';
+import { OccupantRoom } from '../../api/room/OccupantRoom';
+import { Phone } from '../../api/room/Phone';
+
+function buildPerson(user, RoomCollection, PhoneCollection) {
+  const result = {};
+  result.firstName = user.firstName;
+  result.lastName = user.lastName;
+  result.email = user.email;
+  result.position = user.position;
+  result.officeHours = user.officeHours;
+  result.role = user.role;
+  result._id = user._id;
+  let roomArr = OccupantRoom.find({ email: user.email }).fetch();
+  roomArr = roomArr.map(room => room.room);
+  if (roomArr.length === 1) {
+    roomArr = roomArr[0];
+  } else {
+    roomArr = roomArr.join(', ');
+  }
+  console.log(roomArr);
+  let phoneArr = PhoneCollection.find({ email: user.email }).fetch();
+  phoneArr = phoneArr.map(item => item.phoneNum);
+  if (phoneArr.length === 1) {
+    phoneArr = phoneArr[0];
+  } else {
+    phoneArr = phoneArr.join(', ');
+  }
+  console.log(phoneArr);
+  result.room = roomArr;
+  result.phones = phoneArr;
+  return result;
+}
 
 const FacultyInfoBar = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { faculty } = useTracker(() => {
     // Get access to Faculty documents
     const subscription = FacultyProfiles.subscribe();
+    const occRoomSub = OccupantRoom.subscribeOccupantRoom();
+    const phoneSub = Phone.subscribePhone();
     // Determine if the subscription is ready
-    const rdy = subscription.ready();
+    const rdy = subscription.ready() && occRoomSub.ready() && phoneSub.ready();
     // Get the Faculty documents
-    const facultyProfiles = FacultyProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
+    const docFaculty = FacultyProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
+    const testFac = docFaculty[0];
+    console.log(testFac);
+    const fac = buildPerson(testFac, Room, Phone);
+    console.log(fac);
+    console.log(fac.firstName);
     return {
-      faculty: facultyProfiles,
+      faculty: fac,
       ready: rdy,
     };
   }, []);
-  const currentUser = faculty[0];
+  const currentUser = faculty;
 
   return (
     <Card className="rounded-0">
@@ -29,23 +69,11 @@ const FacultyInfoBar = () => {
         <Card.Text>Name: { currentUser.firstName } { currentUser.lastName }</Card.Text>
         <Card.Text>Position: { currentUser.position }</Card.Text>
         <Card.Text>Email: { currentUser.email }</Card.Text>
-        <Card.Text>Office: POST 311</Card.Text>
-        <Card.Text>Office Phone: 808-123-4567</Card.Text>
-        <Card.Text>Office Hours: M 3PM-5PM | W 11AM-1PM</Card.Text>
+        <Card.Text>Office: POST { currentUser.room }</Card.Text>
+        <Card.Text>Office Phone: { currentUser.phones }</Card.Text>
+        <Card.Text>Office Hours: { currentUser.officeHours }</Card.Text>
         <Card.Text>Zoom Link: https://zoom.link</Card.Text>
       </Card.Body>
-      <Card.Header className="rounded-0 dashboard-header">
-        <Card.Title>My Schedule</Card.Title>
-      </Card.Header>
-      <Card.Body>
-        <Card.Text>Mon: <br /> 9:30AM-10:30AM | ICS 211 | HOLMES 215 <br /> 3:00PM-5:00PM | OFFICE HRS | POST 311 </Card.Text>
-        <Card.Text>Wed: <br /> 9:30AM-10:30AM | ICS 211 | HOLMES 215 <br /> 11:00AM-1:00PM | OFFICE HRS | POST 311</Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush rounded-0">
-        <ListGroup.Item>
-          <Card.Link href="#">View campus map</Card.Link>
-        </ListGroup.Item>
-      </ListGroup>
     </Card>
   );
 };
--- app/imports/ui/components/FacultyMySpaces.jsx
+++ app/imports/ui/components/FacultyMySpaces.jsx
@@ -1,42 +1,71 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Card } from 'react-bootstrap';
+import { FacultyProfiles } from '../../api/user/FacultyProfileCollection';
+import { OccupantRoom } from '../../api/room/OccupantRoom';
+import { Phone } from '../../api/room/Phone';
+import FacultyMySpacesCard from './FacultyMySpacesCard';
+import { PortRoom } from '../../api/room/PortRoom';
 
-const FacultyMySpaces = () => (
-  /** something to do with room collections here */
+function buildOffices(user, rooms) {
+  const result = [];
+  for (let i = 0; i < rooms.length; i++) {
+    const resultItem = {};
+    resultItem.room = rooms[i].room;
+    const roomNum = rooms[i].room;
+    const phNum = Phone.find({ room: roomNum }, {}).fetch();
+    const portsArray = PortRoom.find({ room: roomNum }, {}).fetch();
+    if (portsArray.length === 0) {
+      resultItem.ports = 'N/A';
+    } else if (portsArray.length === 1) {
+      resultItem.ports = portsArray[0];
+    } else {
+      resultItem.ports = portsArray.join(', ');
+    }
+    resultItem.phone = phNum.length > 0 ? (phNum[0].phoneNum) : 'N/A';
+    resultItem.hours = user.officeHours;
+    resultItem._id = rooms[i]._id;
+    result[i] = resultItem;
+  }
+  return result;
+}
 
-  <Card className="rounded-0">
-    <Card.Header className="rounded-0 dashboard-header">
-      <Card.Title><h1>My Spaces</h1></Card.Title>
-    </Card.Header>
-    <Card>
-      <Card.Body>
-        <Card.Img src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081" className="shadow-4 float-start m-4" style={{ width: '150px' }} />
-        <Card.Title>POST 311</Card.Title>
-        <Card.Text>
-          Type: Office<br />
-          Phone Number: 808-123-4567<br />
-          Equipment:<br />
-          Capacity:<br />
-          My Occupancy Hours:<br />
-        </Card.Text>
-      </Card.Body>
+const FacultyMySpaces = () => {
+
+  const { offices } = useTracker(() => {
+    // Get access to Faculty documents
+    const subscription = FacultyProfiles.subscribe();
+    const occRoomSub = OccupantRoom.subscribeOccupantRoom();
+    const phoneSub = Phone.subscribePhone();
+    const portSub = PortRoom.subscribePortRoom();
+    // Determine if the subscription is ready
+    const rdy = subscription.ready() && occRoomSub.ready() && phoneSub.ready() && portSub.ready();
+    // Get the Faculty documents
+    let docFaculty = FacultyProfiles.find({ userID: Meteor.user()._id }, {}).fetch();
+    docFaculty = docFaculty[0];
+    // const email = docFaculty.email;
+    // console.log(email);
+    const docRooms = OccupantRoom.find({ email: docFaculty.email }, {}).fetch();
+    console.log(docFaculty);
+    console.log(docFaculty.email);
+    console.log(docRooms);
+    const off = buildOffices(docFaculty, docRooms);
+    console.log(off);
+    return {
+      offices: off,
+      ready: rdy,
+    };
+  }, []);
+
+  return (
+    <Card className="rounded-0">
+      <Card.Header className="rounded-0 dashboard-header">
+        <Card.Title><h1>My Spaces</h1></Card.Title>
+      </Card.Header>
+      { offices.length === 0 ? (<p>NO SPACES</p>) : offices.map((office) => <FacultyMySpacesCard key={office._id} office={office} />)}
     </Card>
-    <Card>
-      <Card.Body>
-        <Card.Img src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081" className="shadow-4 float-start m-4" style={{ width: '150px' }} />
-        <Card.Title>HOLMES 215</Card.Title>
-        <Card.Text>
-          Type: Classroom<br />
-          Phone Number: 808-234-5678<br />
-          Equipment:<br />
-          Capacity: 50 pax<br />
-          My Occupancy Hours: MW 9:30AM-10:30AM<br />
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card.Footer>
-      <a href="/home" className="btn btn-primary">Go somewhere</a>
-    </Card.Footer>
-  </Card>
-);
+  );
+};
+
 export default FacultyMySpaces;
